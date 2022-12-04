44f9e1fb55af33e8839ac312500e2122557a461a
wanglibin8801 <wanglibin8801@zto.com>
2022-12-04 22:49:44 +0800

    update:修改样式

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,13 +2,10 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!11111" );
-        System.out.println( "Hello World!11111" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!11111");
+        System.out.println("Hello World!11111");
     }
 }
